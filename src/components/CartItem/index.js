8d48c8e65cadfad49f1a03f32c1e565340cecd57
kusaljay <kufe28@gmail.com>
2019-08-27 18:34:49 +1000

    Cartitem title in Cart

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -4,7 +4,7 @@ const CartItem = props => {
   return (
     <div>
       <span>{props.cartItemId}</span>
-      {/* <span>{props.cartItemTitle}</span> */}
+      <span>{props.cartItemTitle}</span>
       <button type="button" onClick={props.removeItem}>remove</button>
       <span>{props.cartItemQuantity}</span>
       <input type="text" name="quantity" />
